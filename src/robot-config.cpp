ae06e1c7e19ce733ed460d1aceaccbddafd09987
romanobro56 <romanp2929@gmail.com>
2023-01-13 19:29:14 -0500

    fixed motors 2

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -8,15 +8,15 @@ using code = vision::code;
 brain  Brain;
 
 // VEXcode device constructors
-motor leftMotorA = motor(PORT11, ratio18_1, true);
-motor leftMotorB = motor(PORT12, ratio18_1, true);
+motor leftMotorA = motor(PORT12, ratio18_1, true);
+motor leftMotorB = motor(PORT1, ratio18_1, true);
 motor_group LeftDriveSmart = motor_group(leftMotorA, leftMotorB);
-motor rightMotorA = motor(PORT19, ratio18_1, false);
-motor rightMotorB = motor(PORT20, ratio18_1, false);
+motor rightMotorA = motor(PORT20, ratio18_1, false);
+motor rightMotorB = motor(PORT10, ratio18_1, false);
 motor_group RightDriveSmart = motor_group(rightMotorA, rightMotorB);
 drivetrain Drivetrain = drivetrain(LeftDriveSmart, RightDriveSmart, 319.19, 295, 40, mm, 1);
-motor Roller = motor(PORT18, ratio36_1, false);
-motor Intake = motor(PORT9, ratio36_1, false);
+motor Roller = motor(PORT2, ratio36_1, false);
+motor Intake = motor(PORT7, ratio36_1, false);
 
 // VEXcode generated functions
 
